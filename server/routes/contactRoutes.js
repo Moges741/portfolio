3862643c6b935b490fd61f432bdfbdf9567b39c0
Moges <mogesse741@gmial.com>
2026-01-25 12:12:07 +0300

    fixed contact

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -1,8 +1,26 @@
+// import express from "express";
+// import { sendContact } from "../controllers/contactController.js";
+
+// const router = express.Router();
+
+// router.post("/", sendContact);
+
+// export default router;
+
 import express from "express";
 import { sendContact } from "../controllers/contactController.js";
 
 const router = express.Router();
 
+// GET endpoint for testing
+router.get("/", (req, res) => {
+  res.json({ 
+    message: "Contact API is working",
+    method: "Use POST to submit contact form"
+  });
+});
+
+// POST endpoint
 router.post("/", sendContact);
 
 export default router;
